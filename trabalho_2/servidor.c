4d42d2b0bdfa7b35720714132a08643a92be0807
Lucca Ferreira Paiva <luccap@motorola.com>
2023-09-02 12:37:54 -0300

    exercicio 6

--- trabalho_2/servidor.c
+++ trabalho_2/servidor.c
@@ -12,20 +12,19 @@
 #define LISTENQ 10
 #define MAXDATASIZE 100
 
-
-int get_available_port() {
+int get_available_port(struct sockaddr_in *addr) {
     for (int port = 1024; port <= 65535; port++) {
         int sockfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sockfd < 0) {
             continue;
         }
 
-        struct sockaddr_in addr;
-        addr.sin_family = AF_INET;
-        addr.sin_port = htons(port);
-        addr.sin_addr.s_addr = INADDR_ANY;
+        // bzero(&addr, sizeof(addr)); // This line is no longer needed
+        addr->sin_family = AF_INET;
+        addr->sin_port = htons(port);
+        addr->sin_addr.s_addr = INADDR_ANY;
 
-        if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
+        if (bind(sockfd, (struct sockaddr *)addr, sizeof(*addr)) == 0) {
             close(sockfd);
             return port;
         }
@@ -38,32 +37,24 @@ int get_available_port() {
 
 int main (int argc, char **argv) {
     int    listenfd, connfd;
-    struct sockaddr_in servaddr;
+    struct sockaddr_in servaddr, addr;
     char   buf[MAXDATASIZE];
     time_t ticks;
 
-    int port;
-
-
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
-    }
-
-    port = get_available_port();
+    int port = get_available_port(&addr);
 
     if (port == -1){
         printf("No available ports found\n");
         return 1;
     }
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family      = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port        = htons(port);   
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd == -1) {
+        perror("socket");
+        exit(1);
+    }
 
-    printf("Using port %d\n", port);
-    printf("IP  %d\n", servaddr.sin_addr.s_addr);
+    servaddr = addr;
 
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
         perror("bind");
@@ -75,6 +66,9 @@ int main (int argc, char **argv) {
         exit(1);
     }
 
+    printf("IP: %d\n", inet_ntoa(servaddr.sin_addr));
+    printf("port: %d\n", port);
+
     for ( ; ; ) {
       if ((connfd = accept(listenfd, (struct sockaddr *) NULL, NULL)) == -1 ) {
         perror("accept");
@@ -85,6 +79,15 @@ int main (int argc, char **argv) {
         snprintf(buf, sizeof(buf), "Hello from server!\nTime: %.24s\r\n", ctime(&ticks));
         write(connfd, buf, strlen(buf));
 
+        struct sockaddr_in cliaddr;
+        socklen_t clilen = sizeof(cliaddr);
+        if (getpeername(connfd, (struct sockaddr *)&cliaddr, &clilen) == -1) {
+            perror("getpeername");
+            exit(1);
+        }
+
+        printf("Client IP: %d\n", inet_ntoa(cliaddr.sin_addr));
+        printf("Client Port: %d\n", ntohs(cliaddr.sin_port));
 
         close(connfd);
     }
