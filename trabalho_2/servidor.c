f4e0edf57c4d559dc02fc87ca5a5dae8dcb29671
Lucca Ferreira Paiva <luccap@motorola.com>
2023-09-02 11:24:21 -0300

    Exercicio 4

--- trabalho_2/servidor.c
+++ trabalho_2/servidor.c
@@ -12,11 +12,37 @@
 #define LISTENQ 10
 #define MAXDATASIZE 100
 
+
+int get_available_port() {
+    for (int port = 1024; port <= 65535; port++) {
+        int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0) {
+            continue;
+        }
+
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(port);
+        addr.sin_addr.s_addr = INADDR_ANY;
+
+        if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == 0) {
+            close(sockfd);
+            return port;
+        }
+
+        close(sockfd);
+    }
+
+    return -1;
+}
+
 int main (int argc, char **argv) {
     int    listenfd, connfd;
     struct sockaddr_in servaddr;
     char   buf[MAXDATASIZE];
     time_t ticks;
+
+    int port;
 
 
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -24,10 +50,20 @@ int main (int argc, char **argv) {
         exit(1);
     }
 
+    port = get_available_port();
+
+    if (port == -1){
+        printf("No available ports found\n");
+        return 1;
+    }
+        
+
+    printf("Using port %d\n", port);
+
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family      = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port        = htons(1025);   
+    servaddr.sin_port        = htons(port);   
 
     if (bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
         perror("bind");
